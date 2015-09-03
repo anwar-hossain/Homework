291df479f3e6ffdacf8ee80e2e8dd7225f9017bc
anwar-hossain <anwarhossain2310@gmail.com>
2015-09-03 11:45:47 +0600

    Create hw_1.3.php

---
+++ HW1/hw_1.3.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>program 3</title>
+    </head>
+    <body>
+        <?php 
+        for($num=1; $num<=20000; $num++)
+        {
+        if($num%7===0){
+            echo "$num";
+            echo"<br/>";
+        }
+        
+        }
+        ?>
+    </body>
+</html>
